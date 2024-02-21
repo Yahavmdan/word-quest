69fdb15cad0807c2ab4e8d113205203beb3e8044
yahavdan <yahavmdan@gmail.com>
2024-02-21 22:35:37 +0200

    ongoign

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,46 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { fade, glideY } from "../../shared/utilities/animations";
 import { WordService } from "../../shared/services/word.service";
 import { Word } from "../../shared/types/word";
+import { ThemeService } from "../../shared/services/theme.service";
+import { delay } from "rxjs";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [fade('fade', 500),
-    glideY('glide', -20, 500)]
+  animations: [
+    fade('fade', 100),
+    glideY('glide', -50, 100)
+  ]
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  public showGuide: boolean;
   public word: Word;
   public step: number = 0;
   public hints: string[] = [];
   public level: 'Easy' | 'Medium' | 'Hard' = null;
   public success: boolean;
+  public isDarkMode: boolean;
 
-  constructor(private _wordService: WordService) {
+  constructor(private _wordService: WordService,
+              private themeService: ThemeService) {
+  }
+
+  public ngOnInit(): void {
+    this.themeService.isDarkMode$.subscribe((isDarkMode) => {
+      this.isDarkMode = isDarkMode;
+    });
   }
 
   private getWord(): void {
     this._wordService.getWord(this.level).subscribe(res => {
       this.word = res[0];
-      this.setWord();
+      // this.setWord();
     })
   }
 
-  public chooseDifficulty(level: 'Easy' | 'Medium' | 'Hard', input: HTMLInputElement): void {
+  public chooseDifficulty(level: HTMLDivElement, input: HTMLInputElement): void {
     this.reset(true, input);
-    this.level = level;
-    const word = this.findWord();
-    if (word) {
-      this.word = word;
-    } else {
+    this.level = level.innerHTML as 'Easy' | 'Medium' | 'Hard';
+    // const word = this.findWord();
+    // if (word) {
+    //   this.word = word;
+    // } else {
       this.getWord();
-    }
+    // }
   }
 
   public submit(input: HTMLInputElement): void {
-    this.step ++;
+    this.step++;
     this.failSteps(input);
   }
 
@@ -52,15 +64,15 @@ export class HomeComponent {
     return false;
   }
 
-  private alterElements(success: boolean, input: HTMLInputElement):void {
+  private alterElements(success: boolean, input: HTMLInputElement): void {
     input.nextElementSibling.classList.remove('d-none');
-    input.nextElementSibling.classList.add( success ? 'bi-check-lg' : 'bi-exclamation-lg');
+    input.nextElementSibling.classList.add(success ? 'bi-check-lg' : 'bi-exclamation-lg');
     input.classList.add(success ? 'success' : 'error');
     setTimeout(() => {
       input.nextElementSibling.classList.add('d-none');
       input.nextElementSibling.classList.remove(success ? 'bi-check-lg' : 'bi-exclamation-lg');
       input.classList.remove(success ? 'success' : 'error');
-    }, 1000)
+    }, 500)
   }
 
   private failSteps(input: HTMLInputElement): void {
@@ -77,8 +89,32 @@ export class HomeComponent {
     }
 
     if (this.step === 3) {
-
+      let examples: string[] = []
+      this.word.meanings[0].definitions.forEach(definition => {
+        if (definition['example']) {
+          examples.push(definition['example']);
+        }
+      });
+      this.word.meanings[1]?.definitions.forEach(definition => {
+        if (definition['example']) {
+          examples.push(definition['example']);
+        }
+      });
+      if (examples.length) {
+        this.hints.push(`Hint- ${this.step}: The word can be used in a sentence like this: ${
+          this.replaceWordWithStars(examples[0])
+        }`);
+      }
     }
+  }
+
+  private replaceWordWithStars(definition: string): string {
+    if (!definition) {
+      return '';
+    }
+    const wordToReplace = this.word.word;
+    const stars = '*'.repeat(wordToReplace.length);
+    return definition.replace(wordToReplace, stars);
   }
 
   private reset(moveLevel: boolean, input?: HTMLInputElement): void {
@@ -105,5 +141,8 @@ export class HomeComponent {
     localStorage.setItem(`daly ${this.level} word`, JSON.stringify(this.word))
   }
 
+  public toggleShowGuide(): void {
+    this.showGuide = !this.showGuide;
+  }
 
 }
